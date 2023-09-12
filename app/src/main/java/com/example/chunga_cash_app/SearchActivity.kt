ca2c33da17124913b2372d60aaa737cd1c488d67
Darren Harris <theharriscode401@gmail.com>
2023-09-12 02:16:32 -0400

    getting search input and displaying found data

--- app/src/main/java/com/example/chunga_cash_app/SearchActivity.kt
+++ app/src/main/java/com/example/chunga_cash_app/SearchActivity.kt
@@ -9,6 +9,7 @@ import com.example.chunga_cash_app.databinding.ActivitySearchBinding
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.ValueEventListener
 
 //import kotlinx.android.synthetic.main.activity_main.*
@@ -16,11 +17,22 @@ import com.google.firebase.database.ValueEventListener
 
 class SearchActivity : Activity() {
     private lateinit var binding: ActivitySearchBinding
+    private lateinit var databaseReference: DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySearchBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        val adminNumInput = intent.getStringExtra("adminInput").toString()
+        databaseReference = FirebaseDatabase.getInstance().getReference("Students")
+        databaseReference.child(adminNumInput).get().addOnSuccessListener {
+            if(it.exists()) {
+                val name =  it.child("name").value
+                binding.tvname.text = name.toString()
+                binding.tvAdminNumber.text = adminNumInput
+            }
+        }
 //
 //        binding.searchButton.setOnClickListener {
 //            showSearchDialog()
