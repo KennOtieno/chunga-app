9ed5400cbd71efb706739fd2912b4c601a1a87a6
KennOtieno <otiken001@gmail.com>
2023-09-28 15:19:36 +0300

    Setting Activity

---
+++ SettingsActivity.kt
@@ -0,0 +1,41 @@
+import android.os.Bundle
+import android.content.Intent
+import android.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_settings.*
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.laayout_settings_xml) // Change it to the xml layout
+
+        buttonAboutLegal.setOnClickListener { view, ->
+            // Opens AboutLegalActivity when cllcked
+            startActivity(Intent(this, AboutLegalActivity::class.java))
+        }
+
+        buttonAdminAccount.setOnClickListener { view, ->
+            // Opens AdminAccountActivity when selected/ pressed
+            startActivity(Intenet(this, AdminAccountActivity::class.java))
+        }
+
+        buttonAppVersion&Updates.setOnClickListener { view, ->
+            // Opens App Version & Updates Activitty when clicked
+            startActivity(Intent(this, AppVersion& Updates ::class.java))
+        }
+
+        buttonHelpGuides.setOnClickListener { view, ->
+            // Opens HelpGuideActivity when clicked
+            startActivity(Intent(this, HelpGuideActivity::class.java))
+        }
+
+        buttonNotification.setOnListener { view, ->
+            // Opens up NotificationPreferenceActivity when clicked by the user
+            startActivity(intent(this, NotificationActivity::class.java))
+        }
+
+        buttonThemeCustomization.setOnClickListener { view, ->
+            // Opens up ThemeCustomizationActivity when clicked by the user
+            startActivity(Intent(this, ThemeCustomization::class.java))
+        }
+    }
+}
