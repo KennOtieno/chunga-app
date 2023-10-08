7466904e42d8ad65a39a278b999dbb99fe92dada
KennOtieno <otiken001@gmail.com>
2023-10-08 14:34:14 +0300

    Notification Activity.

--- NotificationActivity.kt
+++ NotificationActivity.kt
@@ -1,16 +1,40 @@
 import android.os.Bundle
-import android.appcompatactivity.app.AppCompatActivity
-import android.widget.Checkbox
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.CheckBox
 import android.widget.Spinner
 import android.widget.Switch
 
 class NotificationActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState : Bundle?)
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_notifications) // xml layout
 
-    val pushNotificaionSwitch = findViewById<Switch>(R.id.pushNotificationSwitch)
-    val notificationSoundSpinner = findViewById<Switch>(R.id.notificationSoundSpinner)
-    val messageNotificationCheckBox = findViewById<Switch>(R.id.messageNotificationCheckBox)
-    val UpdateNotificationCheckBox = findViewById<Switch>(R.id.UpdateNotificationCheckBox)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_notification) // xml layout
+
+        val pushNotificationSwitch = findViewById<Switch>(R.id.pushNotificationSwitch)
+        val notificationSoundSpinner = findViewById<Spinner>(R.id.notificationSoundSpinner)
+        val messageNotificationCheckBox = findViewById<CheckBox>(R.id.messageNotificationCheckBox)
+        val updateNotificationCheckBox = findViewById<CheckBox>(R.id.updateNotificationCheckBox)
+
+        // Load notification settings from SharedPreferences and set UI elements accordingly
+        // ...
+
+        // Listen for changes in UI elements and update notification preferences accordingly
+        pushNotificationSwitch.setOnCheckedChangeListener { _, isChecked ->
+            // Handle push notification enable/disable
+            // Update corresponding SharedPreferences value
+        }
+
+        notificationSoundSpinner.setOnItemSelectedListener { /* Handle selected sound */ }
+
+        messageNotificationCheckBox.setOnCheckedChangeListener { _, isChecked ->
+            // Handle message notification preference change
+            // Update corresponding SharedPreferences value
+        }
+
+        updateNotificationCheckBox.setOnCheckedChangeListener { _, isChecked ->
+            // Handle update notification preference change
+            // Update corresponding SharedPreferences value
+        }
+        // Add listeners for other notification preference checkboxes as needed
+    }
 }
