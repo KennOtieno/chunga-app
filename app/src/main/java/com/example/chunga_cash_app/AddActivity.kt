66e76d841584cf8c5141c6ba8d5a7f5acfa129d3
KennOtieno <otiken001@gmail.com>
2023-10-16 05:55:51 +0300

    New & Corrected version of add activity still under construction

--- app/src/main/java/com/example/chunga_cash_app/AddActivity.kt
+++ app/src/main/java/com/example/chunga_cash_app/AddActivity.kt
@@ -1,27 +1,39 @@
-package com.example.chunga_cash_app
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.FirebaseDatabase
 
-class AddActivity {
+class AddActivity : AppCompatActivity() {
+    private lateinit var studentNameEditText: EditText
+    private lateinit var admissionNumberEditText: EditText
+    private lateinit var classAndStreamEditText: EditText
+    private lateinit var pinCodeEditText: EditText
+    private lateinit var accountBalanceEditText: EditText
+    private lateinit var mpesaNumberEditText: EditText
+    private lateinit var facePhotoEditText: EditText
 
-//import android.os.Bundle
-//import android.content.Intent
-//import androidx.appcompat.app.AppCompatActivity
-//import android.widget.Button
-//
-//class AddActivity : AppComapatActivity() {
-//    override fun onCreate(savedInstanceState : Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_add) // Replace (R.layout.activity_add) with XML layout file
-//
-//        //  Find register button in bracket with its xml ID then set click Listener
-//        val registerButton = findViewById<Button>(R.id.registerButton) // Replace with xml ID
-//
-//        // Here we shall use the XML register button ID to set Click Listener
-//        registerButton.setOnClickListener {
-//            // When Register Button is pressed it opens student registration activity
-//            // You can navigate to a student registration form and start registering student in to the app
-//            val intent = Intent(this, StudentRegistrationActivity::class.java)
-//            startActivity(intent)
-//        }
-//    }
-//}
+    override fun onCreate(savedInstanceState : Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.Layout.activity_add) // xml layout
+
+        // Initalize Edit Texts
+        studentNameEditText = findViewById(R.id.studentNameEditText)
+        admissionNumberEditText = findViewById(R.id.admissionNumberEditText)
+        classAndStreamEditText = findViewById(R.id.classAndStreamEditText)
+        pinCodeEditText = findViewById(R.id.pinCodeEdiitText)
+        accountBalanceEditText = findViewById(R.id.accountBalanceEditText)
+        facePhotoEditText = findViewById(R.id.facePhotoEditText)
+        mpesaNumberEditText = findViewById(R.id.mpesaNumberEditText)
+
+        val saveButton: Button = findViewById(R.id.saveButton)
+        val cancelButton: Button = findViewById(R.id.cancelButton)
+
+        saveButton.setOnClickListener {
+            // Get user inputs from Initialization
+
+        }
+    }
 }
