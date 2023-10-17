6342056ebdf615d02059c398a279140d7ff6930e
KennOtieno <otiken001@gmail.com>
2023-10-17 11:14:02 +0300

    Notifications Activity

--- NotificationActivity.kt
+++ NotificationActivity.kt
@@ -1,8 +1,10 @@
 import android.os.Bundle
-import android.appcomptactivity.app.AppCompatActivity
+import androidx.appcomptactivity.app.AppCompatActivity
 import android.widget.Button
 import android.content.SharedPreferences
 import androidx.preference.PreferenceManager
+import com.example.chunga_cash_app.PreferencesActivity
+import com.example.chunga_cash_app.SoundActivity
 
 class NotificationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState : Bundle?) {
@@ -10,20 +12,20 @@ class NotificationActivity : AppCompatActivity() {
         setContentView(R.Layout.activity_notification) // xml layout file
 
         val preferencesButton = findViewById<Button>(R.id.preferencesButton)
-        val soundNotificaticationButton = findViewById<Button>(R.id.soundNotificationButton)
+        val soundNotificationButton = findViewById<Button>(R.id.soundNotificationButton)
 
         preferencesButton.setOnClickListener {
             // Opens preferences activity when clicked
-            val intent (this@NotificationActivity, PreferenceActivity::class.java)
+            val intent = (this@NotificationActivity, PreferencesActivity::class.java)
             startActivity(intent)
             // User can select whether he wants to receive updates or Money Notifications(Daraja API)
         }
 
-        soundNotificaticationButton.setONClickListener {
+        soundNotificationButton.setOnClickListener {
             // Opens sound activity when clicked
-            val intent (this@NotificationActivity, SoundActivity::class.java)
+            val intent = (this@NotificationActivity, SoundActivity::class.java)
             startActivity(intent)
-            // User can seect his own message ringtone and whether she wants vibration mode
+            // User can select his own message ringtone and whether she wants vibration mode
         }
     }
 }
