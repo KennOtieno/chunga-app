71783d91cc403f406c6fe81c08fdbd8c8b839cd7
KennOtieno <otiken001@gmail.com>
2023-10-18 11:44:30 +0300

    Notification Activity.

--- NotificationActivity.kt
+++ NotificationActivity.kt
@@ -1,5 +1,5 @@
 import android.os.Bundle
-import androidx.appcomptactivity.app.AppCompatActivity
+import androidx.appcompatactivity.app.AppCompatActivity
 import android.widget.Button
 import android.content.SharedPreferences
 import androidx.preference.PreferenceManager
@@ -9,21 +9,21 @@ import com.example.chunga_cash_app.SoundActivity
 class NotificationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState : Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.Layout.activity_notification) // xml layout file
+        setContentView(R.layout.activity_notification) // xml layout file
 
         val preferencesButton = findViewById<Button>(R.id.preferencesButton)
         val soundNotificationButton = findViewById<Button>(R.id.soundNotificationButton)
 
         preferencesButton.setOnClickListener {
             // Opens preferences activity when clicked
-            val intent = (this@NotificationActivity, PreferencesActivity::class.java)
+            val intent = Intent(this@NotificationActivity, PreferencesActivity::class.java)
             startActivity(intent)
             // User can select whether he wants to receive updates or Money Notifications(Daraja API)
         }
 
         soundNotificationButton.setOnClickListener {
             // Opens sound activity when clicked
-            val intent = (this@NotificationActivity, SoundActivity::class.java)
+            val intent = Intent(this@NotificationActivity, SoundActivity::class.java)
             startActivity(intent)
             // User can select his own message ringtone and whether she wants vibration mode
         }
