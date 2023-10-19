0b8ff85a40d94a947062a8bb5a502f7505ce7a1b
KennOtieno <otiken001@gmail.com>
2023-10-19 16:53:06 +0300

    Preferences Activity still on going 70% done

--- PreferencesActivity.kt
+++ PreferencesActivity.kt
@@ -1,11 +1,11 @@
 import android.os.Bundle
-import android.appcompatactivity.app.AppCompatActivity
+import android.appcompat.app.AppCompatActivity
 import android.widget.Switch
 
 class PreferencesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState : Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.Layout.activity_preferences) // XML layout
+        setContentView(R.layout.activity_preferences) // XML layout
 
         val updatesNotification = findViewById<Switch>(R.id.updatesNotification)
         val mpesaNotification = findViewById<Switch>(R.id.mpesaNotification)
@@ -14,22 +14,50 @@ class PreferencesActivity : AppCompatActivity() {
             // Handles Updates notifictions
             if(isChecked) {
                 // Updates Notifications will be enabled
-                // Logic for this to follow immediately
+                enableUpdatesNotifications()
             } else {
                 // Updates will be disabled and user won't receive Updates Notifications
-                // Logic for this below
+                disableUpdatesNotifications()
             }
         }
 
         mpesaNotification.setOnCheckedChangedListener { _, isChecked ->
-            // Handles M-PESA notification...this is something we shall write the logic code for it
-            // once we have integrated the Daraja API into Chunga App
             if(isChecked) {
                 // Mpesa Notification is enabled
-                // The code Logic for this to be done once the API is integrated
+                enableMpesaNotification()
             } else {
                 // Mpesa Notification is disabled
-                // The code logic for this to be written once the API is integrated
+                disableMpesaNotification()
+            }
+        }
+    }
+    private fun enableUpdatesNotifications() {
+        // Firebase Realtime Database reference
+        val updatesReference = FirebaseDatabase.getInstsnce().getReference("updates")
+
+        // Check for updates in the database
+        updatesReference.addListenerForSingleValueEvent(Object : ValueEventListener) {
+            override fun onDataChange(dataSnapshot : DataSnaphot) {
+                // Check if there is new update data in the database
+                if (dataSnapshot.exists()) {
+                    // Get the latest version code from the database
+                    val latestVersionCode = dataSnapshot.child("versionCode").getValue(Int::class.java)
+
+                    // Compare the latest version with the current version code
+                    val currentVersionCode = BuildConfig.VERSION_CODE // Assuming we got version code in BuildConfig
+                    if (latestVersionCode != null && latestVersionCode > currentVersionCode)
+                        // There is a new version available in the database
+                        // Show a notification to inform the user about the new update
+                        showNotification("New Update Available", "A new Version of Chunga Cash App is available download now to get new and improved features")
+                } else {
+                    // No new version available, you can handle this case if needed
+                } else {
+                    // No update data found in the database
+                }
+            }
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Handle any errors that occur during the dat fetching proccess
+                // I,plement error handling here if I figure it out
             }
         }
     }
