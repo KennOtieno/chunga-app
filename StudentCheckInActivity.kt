5ccfc103a66b9f77ba926e573dfaaa2b19f87b84
KennOtieno <otiken001@gmail.com>
2023-11-05 16:05:40 +0300

    Student Check In Activity Unfinished.

--- StudentCheckInActivity.kt
+++ StudentCheckInActivity.kt
@@ -1,90 +1,77 @@
+package com.example.chunga_cash_app
+
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
-import android.widget.ImageView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.*
+import android.widget.TextView
+import android.widget.Toast
+import android.widget.ImageView
+import com.google.firebaase.database.*
 
-class CheckInActivity : AppCompatActivity() {
-
-    private lateinit var pinCodeEditText: EditText
-    private lateinit var checkInButton: Button
-    private lateinit var backButton: Button
-    private lateinit var forgotPasswordButton: Button
-    private lateinit var photoImageView: ImageView
-    private lateinit var nameTextView: TextView
-    private lateinit var admissionNumberTextView: TextView
-    private lateinit var classTextView: TextView
-
-    private lateinit var firebaseAuth: FirebaseAuth
-    private lateinit var databaseReference: DatabaseReference
-
+class StudentCheckInActivity : AppCompatActivity () {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_check_in) // XML LAYOUT
+        setContentView(R.layout.activity_student_check_in) // XML LAYOUT
 
-        // Initialize UI elements
-        pinCodeEditText = findViewById(R.id.pinCodeEditText)
-        checkInButton = findViewById(R.id.checkInButton)
-        backButton = findViewById(R.id.backButton)
-        forgotPasswordButton = findViewById(R.id.forgotPasswordButton)
-        photoImageView = findViewById(R.id.photoImageView)
-        nameTextView = findViewById(R.id.nameTextView)
-        admissionNumberTextView = findViewById(R.id.admissionNumberTextView)
-        classTextView = findViewById(R.id.classTextView)
+        // Intialize firebase database reference
+        databaseReference = FirebaseDatabase.getInstance().reference.child("students")
 
-        // Initialize Firebase
-        firebaseAuth = FirebaseAuth.getInstance()
-        databaseReference = FirebaseDatabase.getInstance().getReference("students")
+        // Fetch student data from database
+        databaseReference.child("student_key").addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    val student = dataSnapshot.getValue(Student::class.java)
 
-        // Retrieve student details from Firebase based on user's authentication ID
-        val userId = firebaseAuth.currentUser?.uid
-        userId?.let {
-            databaseReference.child(it).addListenerForSingleValueEvent(object : ValueEventListener {
-                override fun onDataChange(snapshot: DataSnapshot) {
-                    if (snapshot.exists()) {
-                        val name = snapshot.child("name").getValue(String::class.java)
-                        val admissionNumber = snapshot.child("admissionNumber").getValue(String::class.java)
-                        val className = snapshot.child("class").getValue(String::class.java)
-                        // Set student details in UI
-                        nameTextView.text = "NAME: $name"
-                        admissionNumberTextView.text = "ADMISSION NO: $admissionNumber"
-                        classTextView.text = "CLASS: $className"
+                    // Display Student Information
+                    val photoImageView: ImageView = findViewById(R.id.photoImageView)
+                    // Load image into photoImageView using student.photo
+                    // I need to do research on picasso or Glide this part is unfinished business.
+
+                    val nameTextView = findViewById<TextView>(R.id.nameTextView)
+                    val adminNumTextView = findViewById<TextView>(R.id.adminNumTextView)
+                    val classStreamTextView = findViewById<TextView>(R.id.classStreamTextView)
+
+                    nameTextView.text = student?.name
+                    adminNumTextView.text = student?.adminNum.toString()
+                    classStreamTextView.text = student?.classStream
+
+                    // Pin Input field and OK button
+                    val pinEditText = findViewById<EditText>(R.id.pinEditText)
+                    val okButton = findViewById<Button>(R.id.okButton)
+
+                    okButton.setOnClickListener {
+                        val enteredPin = pinEditText.text.toString()
+                        val correctPin = student?.pinCode.toString()
+
+                        if (enteredPin == correctPin) {
+                            // If it's the correct pin, navigate to Student Account Activity
+                            val intent = Intent(this@StudentCheckInActivity, StudentAccountActivity::class.java)
+                            startActivity(intent)
+
+                            Toast.makeText(this@StudentCheckInActivity, "Done", Toast.LENGTH_SHORT).show()
+                        }
                     }
-                }
 
-                override fun onCancelled(error: DatabaseError) {
-                    // Handle database error if needed
-                }
-            })
-        }
+                    // Forgot Pin Button ("It shall lead to an activity called 'ForgotPinActivity' when clicked)
+                    val forgotPinCode = findViewById<Button>(R.id.forgotPinCode)
+                    forgotPinCode.setOnClickListener {
+                        // When clicked goes to Forgot Pin Activity
+                        val intent = Intent(this@StudentCheckInActivity, ForgotPinActivity::class.java)
+                        startActivity(intent)
 
-        // Check-in button click listener
-        checkInButton.setOnClickListener {
-            val enteredPin = pinCodeEditText.text.toString()
-            // Validate entered pin with the pin saved in Firebase
-            // Implement your own logic to fetch and validate the pin from Firebase
-            if (enteredPin == "1234") { // Replace "1234" with the correct pin fetched from Firebase
-                // Correct pin, navigate to StudentAccountActivity
-                val intent = Intent(this, StudentAccountActivity::class.java)
-                startActivity(intent)
-            } else {
-                // Incorrect pin, show toast message
-                Toast.makeText(this, "! Wrong Pin Code. Try Again.", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(this@StudentCheckInActivity, "Openning", Toast.LENGTH_SHORT).show()
+                    }
+                } else {
+                    // Handle the case where student data doesn't exist in the database
+                    Toast.makeText(this@StudentCheckInActivity, "Not Found", Toast.LENGTH_SHORT).show()
+                }
             }
-        }
 
-        // Back button click listener
-        backButton.setOnClickListener {
-            finish() // Close this activity and go back to the previous screen
-        }
-
-        // Forgot Password button click listener
-        forgotPasswordButton.setOnClickListener {
-            val intent = Intent(this, StudentForgotPinActivity::class.java)
-            startActivity(intent)
-        }
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Handle Errors
+                Toast.makeText(this@StudentCheckInActivity, "Failed to Retrieve", Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 }
