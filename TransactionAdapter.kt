7afbef445236fad9a1d3c36f8a5656f244b029d9
KennOtieno <otiken001@gmail.com>
2023-11-20 14:20:40 +0300

    Transaction Adapter class file

--- TransactionAdapter.kt
+++ TransactionAdapter.kt
@@ -4,13 +4,37 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import androidx.recylerview.widget.RecylerView
-class TransactionAdapter : RecylerView.Adapter<TransactionAdapter.TransactionViewHolder>() {
+import androidx.recyclerview.widget.RecyclerView
+
+class TransactionAdapter : RecyclerView.Adapter<TransactionAdapter.TransactionViewHolder>() {
 
     private var transactions: List<Transaction> = emptyList()
 
     fun setTransactions(transactions: List<Transaction>) {
         this.transactions = transactions
         notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TransactionViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_transaction, parent, false)
+        return TransactionViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: TransactionViewHolder, position: Int) {
+        val transaction = transactions[position]
+        holder.bind(transaction)
+    }
+
+    override fun getItemCount(): Int {
+        return transactions.size
+    }
+
+    class TransactionViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val messageTextView: TextView = itemView.findViewById(R.id.messageTextView)
+
+        fun bind(transaction: Transaction) {
+            // Customize the binding based on your Transaction class structure
+            messageTextView.text = transaction.message
+        }
     }
 }
