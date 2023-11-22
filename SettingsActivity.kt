63c66f3719e79c9cab23f58b1be4ba6ebf76b53d
KennOtieno <otiken001@gmail.com>
2023-11-22 07:32:32 +0300

    Settings Activity

--- SettingsActivity.kt
+++ SettingsActivity.kt
@@ -1,3 +1,5 @@
+package com.example.chunga_cash_app
+
 import android.os.Bundle
 import android.content.Intent
 import android.appcompat.app.AppCompatActivity
@@ -6,7 +8,7 @@ import kotlinx.android.synthetic.main.activity_settings.*
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.laayout_settings_xml) // Change it to the xml layout
+        setContentView(R.layout.activity_settings) // xml layout
 
         buttonAboutLegal.setOnClickListener { view, ->
             // Opens AboutLegalActivity when cllcked
@@ -14,11 +16,11 @@ class SettingsActivity : AppCompatActivity() {
         }
 
         buttonAppVersionAndUpdates.setOnClickListener { view, ->
-            // Opens AppVersionAndUpdates Activitty when clicked
-            startActivity(Intent(this, AppVersion& Updates ::class.java))
+            // Opens AppVersionAndUpdates Activity when clicked
+            startActivity(Intent(this, AppVersionAndUpdatesActivity ::class.java))
         }
 
-        buttonHelpGuides.setOnClickListener { view, ->
+        buttonHelpGuide.setOnClickListener { view, ->
             // Opens HelpGuideActivity when clicked
             startActivity(Intent(this, HelpGuideActivity::class.java))
         }
@@ -30,7 +32,7 @@ class SettingsActivity : AppCompatActivity() {
 
         buttonThemeCustomization.setOnClickListener { view, ->
             // Opens up ThemeCustomizationActivity when clicked by the user
-            startActivity(Intent(this, ThemeCustomization::class.java))
+            startActivity(Intent(this, ThemeCustomizationActivity::class.java))
         }
     }
 }
