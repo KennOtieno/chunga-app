ff340ea1dce2799436fb19f2d3c56aea55b2e247
KennOtieno <otiken001@gmail.com>
2023-12-05 06:13:32 +0300

    Preference Activity Incomplete

--- PreferencesActivity.kt
+++ PreferencesActivity.kt
@@ -2,10 +2,16 @@ package com.example.chunga_cash_app
 
 import android.os.Bundle
 import android.widget.Switch
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.google.firebase.database.*
 
 class PreferencesActivity : AppCompatActivity() {
+
+    // Notification ID for Chunga
+    private val NOTIFICATION_ID = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,58 +19,59 @@ class PreferencesActivity : AppCompatActivity() {
 
         val updatesNotification = findViewById<Switch>(R.id.updatesNotification)
 
+        // Check the initial state of the switch
+        if (updatesNotification.isChecked) {
+            enableUpdatesNotifications()
+        }
+
         updatesNotification.setOnCheckedChangeListener { _, isChecked ->
             // Handles Updates notifications
             if (isChecked) {
-                // Updates Notifications will be enabled
+                // Updates Notifications will be enabled & we show a Toast
                 enableUpdatesNotifications()
+                showToast("Updates Enabled")
             } else {
                 // Updates will be disabled, and the user won't receive Updates Notifications
                 disableUpdatesNotifications()
+                showToast("Updates Disabled")
             }
         }
     }
 
+    private fun showToast(message: String) {
+        // Display a Toast message to the user
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
     private fun enableUpdatesNotifications() {
-        // Firebase Realtime Database reference
-        val updatesReference = FirebaseDatabase.getInstance().getReference("updates")
+        // Show a notification to inform the user about the new update
+        val notificationTitle = "New Update Available"
+        val notificationMessage = "A new version of Chunga Cash App is available. UPDATE NOW to get new improved features."
 
-        // Check for updates in the database
-        updatesReference.addListenerForSingleValueEvent(object : ValueEventListener {
-            override fun onDataChange(dataSnapshot: DataSnapshot) {
-                // Check if there is new update data in the database
-                if (dataSnapshot.exists()) {
-                    // Get the latest version code from the database
-                    val latestVersionCode = dataSnapshot.child("versionCode").getValue(Int::class.java)
-
-                    // Compare the latest version with the current version code
-                    val currentVersionCode = BuildConfig.VERSION_CODE // Assuming we got version code in BuildConfig
-                    if (latestVersionCode != null && latestVersionCode > currentVersionCode) {
-                        // There is a new version available in the database
-                        // Show a notification to inform the user about the new update
-                        showNotification("New Update Available", "A new version of Chunga Cash App is available. Download now to get new and improved features.")
-                    } else {
-                        // No new version available, you can handle this case if needed
-                    }
-                } else {
-                    // No update data found in the database
-                }
-            }
-
-            override fun onCancelled(databaseError: DatabaseError) {
-                // Handle any errors that occur during the data fetching process
-                // Implement error handling here if needed
-            }
-        })
+        // Call the function to display the notification
+        showNotification(notificationTitle, notificationMessage)
     }
 
     private fun showNotification(title: String, message: String) {
-        // Implement code to create and display a notification using Android's NotificationManager
-        // This function would typically involve creating a NotificationCompat.Builder and displaying the notification
-        // Implement the necessary code here for showing the notification to the user
+        // Create and display a notification using Android's NotificationManager
+        val builder = NotificationCompat.Builder(this, "channel_id")
+            .setSmallIcon(R.drawable.notification_icon)
+            .setContentTitle(title)
+            .setContentText(message)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+        // NotificationManager is a system service that manages notifications
+        with(NotificationManagerCompat.from(this)) {
+            // Use the notification ID to update or cancel the notification later
+            notify(NOTIFICATION_ID, builder.build())
+        }
     }
 
     private fun disableUpdatesNotifications() {
         // Logic to disable Updates Notifications if needed
+        // You may cancel the existing notification here if needed
+        with(NotificationManagerCompat.from(this)) {
+            cancel(NOTIFICATION_ID)
+        }
     }
 }
