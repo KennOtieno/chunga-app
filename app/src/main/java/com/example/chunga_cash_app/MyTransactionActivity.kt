051e6d68cecf65aa08af2c5a527d4f3c69304e09
Darren Harris <theharriscode401@gmail.com>
2023-12-18 07:18:52 -0500

    more new xml files & data binding

--- app/src/main/java/com/example/chunga_cash_app/MyTransactionActivity.kt
+++ app/src/main/java/com/example/chunga_cash_app/MyTransactionActivity.kt
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.database.*
+import android.widget.Toast
+import com.example.chunga_cash_app.databinding.ActivityMyTransactionBinding
 
 class MyTransactionActivity : AppCompatActivity() {
 
@@ -12,16 +14,19 @@ class MyTransactionActivity : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
     private lateinit var transactionAdapter: TransactionAdapter
     private var studentKey: String = "adminNumText" // This is the actual key on the DB
+    private lateinit var binding: ActivityMyTransactionBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_my_transaction)
+        binding = ActivityMyTransactionBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
         // Initialize Firebase Database Reference
         databaseReference = FirebaseDatabase.getInstance().reference.child("students").child(studentKey)
 
         // Initialize RecyclerView
-        recyclerView = findViewById(R.id.transactionRecyclerView)
+        recyclerView = binding.transactionRecyclerView
         recyclerView.layoutManager = LinearLayoutManager(this)
         transactionAdapter = TransactionAdapter()
         recyclerView.adapter = transactionAdapter
