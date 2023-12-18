c2dcb7c482189a9e6bed3beaa056be0482bbbf30
Darren Harris <theharriscode401@gmail.com>
2023-12-18 16:04:40 -0500

    imported Transaction class

--- app/src/main/java/com/example/chunga_cash_app/TransactionAdapter.kt
+++ app/src/main/java/com/example/chunga_cash_app/TransactionAdapter.kt
@@ -5,6 +5,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.database.Transaction
 
 class TransactionAdapter : RecyclerView.Adapter<TransactionAdapter.TransactionViewHolder>() {
 
