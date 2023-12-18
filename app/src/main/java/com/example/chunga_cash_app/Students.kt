65276b199b042899ddddd20431682903be775d9f
Darren Harris <theharriscode401@gmail.com>
2023-12-18 15:39:35 -0500

    data binding || xml files for activity student account & app version/updates

--- app/src/main/java/com/example/chunga_cash_app/Students.kt
+++ app/src/main/java/com/example/chunga_cash_app/Students.kt
@@ -1,6 +1,6 @@
 package com.example.chunga_cash_app
 
-data class Students(val name: String? = null, val adminNum: Int? = null, val classStream: String? = null, val pinCode:Int? = null, val accountBalance:Int? = null,
+data class Students(val name: String? = null, val adminNum: Int? = null, val classStream: String? = null, val pinCode:Int? = null, var accountBalance:Int? = null,
                     val photo: String? = null) {
 
 }
