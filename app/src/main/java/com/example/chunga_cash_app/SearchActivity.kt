2283676783de426f82dbdb8feeb29a617339e9a1
KennOtieno <otiken001@gmail.com>
2023-12-21 09:33:14 +0300

    Search Activity Kotlin Code File.

--- app/src/main/java/com/example/chunga_cash_app/SearchActivity.kt
+++ app/src/main/java/com/example/chunga_cash_app/SearchActivity.kt
@@ -1,26 +1,59 @@
 package com.example.chunga_cash_app
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.database.*
 
 class SearchActivity : AppCompatActivity() {
 
     private lateinit var databaseReference: DatabaseReference
+    private lateinit var searchEditText: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
 
         databaseReference = FirebaseDatabase.getInstance().getReference("Students")
-        val adminNumInput = intent.getStringExtra("adminInput").toString()
+        searchEditText = findViewById(R.id.editTextAdminNumber)
+        val okButton: Button = findViewById(R.id.buttonOk)
 
-        databaseReference.child(adminNumInput).get().addOnSuccessListener { snapshot ->
-            if (snapshot.exists()) {
-                val name = snapshot.child("name").value
-                val adminNumber = snapshot.key
-                // Update your UI elements with name and adminNumber
+        okButton.setOnClickListener {
+            val adminNumInput = searchEditText.text.toString().trim()
+
+            if (adminNumInput.isNotEmpty()) {
+                checkAdmissionNumber(adminNumInput)
+            } else {
+                showToast("Please enter Admission Number")
             }
         }
+    }
+
+    private fun checkAdmissionNumber(adminNumInput: String) {
+        databaseReference.child(adminNumInput).addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if (snapshot.exists()) {
+                    // Admission Number exists, go to ShowStudentDetails activity
+                    val intent = Intent(this@SearchActivity, ShowStudentDetails::class.java)
+                    intent.putExtra("adminNumber", adminNumInput)
+                    startActivity(intent)
+                    finish()
+                } else {
+                    // Admission Number does not exist, show a toast
+                    showToast("Admission Number does not exist")
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                showToast("Error checking Admission Number: ${error.message}")
+            }
+        })
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
