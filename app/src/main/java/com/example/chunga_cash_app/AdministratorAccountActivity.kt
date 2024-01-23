03c393d6d4256c2f04c50a541e8e0b68dcaffdb5
KennOtieno <otiken001@gmail.com>
2024-01-23 09:59:04 +0300

    Administrator Account Activity

--- app/src/main/java/com/example/chunga_cash_app/AdministratorAccountActivity.kt
+++ app/src/main/java/com/example/chunga_cash_app/AdministratorAccountActivity.kt
@@ -1,35 +1,34 @@
 package com.example.chunga_cash_app
 
 import android.app.Activity
-
-mport android.app.Activity
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import android.widget.Button
 import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import com.example.chunga_cash_app.databinding.ActivityAdminAccountBinding
 
-class AdministratorAccountActivity : Activity() {
+class AdministratorAccountActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityAdminAccountBinding
-    override fun onCreate(savedInstanceState : Bundle?) {
+    private lateinit var binding: ActivityAdminAccountBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAdminAccountBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view) // XML layout file
 
-        val enrollmentAndSavings = findViewById<Button>(R.id.changeDetails)
-        val changeDetails = findViewById<Button>(R.id.enrollmentAndSavings)
+        val changeDetails = findViewById<Button>(R.id.changeDetails)
+        val enrollmentAndSavings = findViewById<Button>(R.id.enrollmentAndSavings)
 
         // set the buttons On Click Listener
-        binding.changeDetails.setOnClickListener {
+        changeDetails.setOnClickListener {
             // When clicked it opens up 'Change Details Activity'
             // User can change the Admin Data Class details
             val intent = Intent(this, ChangeDetailsActivity::class.java)
             startActivity(intent)
         }
 
-        binding.enrollmentAndSavings.setOnClickListener {
+        enrollmentAndSavings.setOnClickListener {
             // When clicked it opens up the 'Enrollment And Savings Activity'
             // User can see total number of Students Enrolled in the app & Total Amount Saved
             val intent = Intent(this, EnrollmentAndSavingsActivity::class.java)
